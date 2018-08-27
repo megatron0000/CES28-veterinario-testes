857271a6e42536e80286896270263228b1609a37
aluno <aluno>
2018-08-27 14:42:39 -0300

    Tests running

--- src/core/Dummy.java
+++ src/core/Dummy.java
@@ -1,7 +1,12 @@
 package core;
 
 public class Dummy {
+<<<<<<< HEAD
 	public static void main(String[] args)  {
 		System.out.println("oi");
+=======
+	public static void main(String[] args) {
+		System.out.print("Hello world");
+>>>>>>> Tests running
 	}
 }
--- test/test/Dummy.java
+++ test/test/Dummy.java
@@ -1,5 +1,29 @@
 package test;
+import org.junit.*;
 
 public class Dummy {
-
+    @BeforeClass
+    public static void setUpClass() {
+// code executed before all test methods
+    }
+    @Before
+    public void setUp() {
+// code executed before each test method
+    }
+    @Test
+    public void testAdd() {
+// test method
+    }
+    @Test
+    public void testSubtract() {
+// test method
+    }
+    @After
+    public void tearDown() {
+// code executed after each test method
+    }
+    @AfterClass
+    public static void tearDownClass() {
+// code executed after all test methods
+    }
 }
