066beba4831ad5a7d3c4f55b3f8a5b0d49a91e0d
aluno <aluno>
2018-08-27 16:10:39 -0300

    class animal ok

--- src/core/Animal.java
+++ src/core/Animal.java
@@ -1,42 +1,38 @@
 package core;
 
-import java.awt.List;
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class Animal {
 	private String name;
 	private String animaltype;
 	private Client client;
-	private List<Session> sessions;
+	private ArrayList<Session> sessions;
 	
 	public Animal(String name, String animaltype, Client client, Session session) {
 		this.name = name;
 		this.animaltype = animaltype;
 		this.client = client;
-		this.sessions = new List<Session>();
+		this.sessions.add(session);
 	}
 	
 	public void addSession(Session session) {
 		this.sessions.add(session);
 	}
 	
-	public void setSessionDescription(String sessiondescription) {
-		this.sessiondescription = sessiondescription;
+	public String getName() {
+		return this.name;
 	}
 	
-	public Animal getAnimal() {
-		return this.animal;
-	}
-	
-	public String getSessionDescription() {
-		return this.sessiondescription;
-	}
-	
-	public String getTreatment() {
-		return this.treatment;
-	}
-	
-	public String getClient() {
+	public Client getClient() {
 		return this.client;
+	}
+	
+	public String getAnimalType() {
+		return this.animaltype;
+	}
+	
+	public ArrayList<Session> getSessions() {
+		return this.sessions;
 	}
 }
